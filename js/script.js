ff11daebd975232a2f640be2738fa06b4058f21a
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-25 18:19:57 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+
+  const age = parseInt(document.getElementById('age-entered').value)
+
+  if (age >= 18) {
+
+    document.getElementById('answer').innerHTML = 'You can see an R rated movie alone.'
+
+  } else if (age >= 13) {
+
+    document.getElementById('answer').innerHTML = 'You can see a PG-13 movie alone.'
+
+  } else {
+
+    document.getElementById('answer').innerHTML = 'You can see a G rated movie alone.'
+  }
+}
