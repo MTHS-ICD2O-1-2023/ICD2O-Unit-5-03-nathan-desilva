8c991c7e5cfcf1941977c3718ecea8e1995bd78a
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-05-25 22:24:41 +0000

    edited js

--- js/script.js
+++ js/script.js
@@ -4,26 +4,28 @@
 // Created on: April 2024
 // This file contains the JS functions for index.html
 
-function myButtonClicked() {
 
+function myButtonClicked() {
   const age = parseInt(document.getElementById('age-entered').value)
 
-  if (age >= 17) {
+  // Store the age in local storage
+  localStorage.setItem('age', age)
 
+  if (localStorage.age >= 17) {
     document.getElementById('answer').innerHTML = 'You can see an R rated movie alone.'
-
-  } else if (age >= 13) {
-
+  } else if (localStorage.age >= 13) {
     document.getElementById('answer').innerHTML = 'You can see a PG-13 movie alone.'
-
-  } else if (age >= 9) {
-
+  } else if (localStorage.age >= 9) {
     document.getElementById('answer').innerHTML = 'You can see a G or PG rated movie alone.'
-
-  } else if (age >= 6) {
-
+  } else if (localStorage.age >= 6) {
     document.getElementById('answer').innerHTML = 'You can see a G rated movie alone.'
-
   }
-
 }
+
+// Retrieve the stored age from local storage on page load
+window.addEventListener('load', () => {
+  const storedAge = localStorage.getItem('age')
+
+  // Set the input value with the stored age
+  document.getElementById('age-entered').value = storedAge || ''
+})
